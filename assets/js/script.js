6532da67af32e29295afdd706b55a76a8f560975
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-06-07 12:19:22 +0200

    add potions array and game board

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var potions = [ "Blue", "Pink", "Green", "Yellow", "Red", "Purple" ];
+var board = [];
+var row = 9;
+var columns = 9;
+var score = 0;
+
+window.onload = function () {
+    startGame();
+};
+
+function startGame() {
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            
+        }
+    }
+}
