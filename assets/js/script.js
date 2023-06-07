2af617ce9ffbf760a75047f1d4ded9c3662298fb
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-06-07 12:36:38 +0200

    add random potions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,24 @@ window.onload = function () {
     startGame();
 };
 
+function randomPotion() {
+    return potions[ Math.floor(Math.random() * potions.length) ]; //0 - 5.99
+}
+
 function startGame() {
-    for (let r = 0; r < rows; r++) {
+    for (let r = 0; r < row; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
-            
+            // <img id="0-0" src="./images/Red.png">
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString;
+            tile.src = "./images/" + randomPotion() + ".png";
+
+            document.getElementById("board").append(tile);
+            row.push(tile);
         }
+        board.push(row);
     }
+
+    console.log(board);
 }
