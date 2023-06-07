2321b65c64d02bfa067b487d15a35024c1e904bc
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-06-07 13:32:20 +0200

    add dragEnd function swapping potions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ var row = 9;
 var columns = 9;
 var score = 0;
 
+var currTile;
+var otherTile;
+
 window.onload = function () {
     startGame();
 };
@@ -35,4 +38,54 @@ function startGame() {
     }
 
     console.log(board);
+}
+
+function dragStart() {
+    //"this" refers to the tile to be clicked on for dragging
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    //"this" refers to the other tile that was dropped on
+    otherTile = this;
+}
+
+function dragEnd() {
+
+    let currCoords = currTile.id.split("-"); //id="0-0" -> ["0", "0"]
+    let r = parseInt(currCoords[ 0 ]);
+    let c = parseInt(currCoords[ 1 ]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[ 0 ]);
+    let c2 = parseInt(otherCoords[ 1 ]);
+
+    let moveLeft = c2 == c - 1 && r == r2;
+    let moveRight = c2 == c + 1 && r == r2;
+
+    let moveUp = r2 == r - 1 && c == c2;
+    let moveDown = r2 == r + 1 && c == c2;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+
+
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+        currTile.src = otherImg;
+        otherTile = currImg;
+    }
 }
